b511e1eb36af74290a5ba9089e603a930546c640
stakedancecard <alanlbird@gmail.com>
2018-12-21 07:29:30 -0700

    Added Ionic Framework

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,6 @@
 import { h, Component } from "preact";
 import { Router } from "preact-router";
+import Helmet from "preact-helmet";
 
 // Code-splitting is automated for routes
 import Home from "../routes/home";
@@ -18,10 +19,26 @@ export default class App extends Component {
   render() {
     return (
       <div id="app">
+        <Helmet
+          title="Ward Bulletin"
+          link={[
+            {
+              rel: "stylesheet",
+              href:
+                "https://unpkg.com/@ionic/core@4.0.0-rc.0/css/ionic.bundle.css"
+            }
+          ]}
+          script={[
+            {
+              src: "https://unpkg.com/@ionic/core@4.0.0-rc.0/dist/ionic.js",
+              type: "text/javascript"
+            }
+          ]}
+        />
         <Router onChange={this.handleRoute}>
-          {/* <Home path="/" /> */}
-          <Bulletin path="/" />
+          <Home path="/" />
           <Editor path="/editor/" />
+          <Bulletin path="/bulletin/" />
           <Bulletin path="/bulletin/:unit" />
         </Router>
       </div>
