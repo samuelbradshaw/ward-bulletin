49e6778dd2eef7307ace212d823cbb828429048c
stakedancecard <alanlbird@gmail.com>
2019-02-07 13:22:32 -0800

    Login, auth

---
+++ src/data/firebase.js
@@ -0,0 +1,76 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseui from "firebaseui";
+import "firebaseui";
+
+let ui;
+
+var config = {
+  apiKey: "AIzaSyCSnrK3X_4pzbaieU-Qm5QzuD2CGOn5o3k",
+  authDomain: "ward-bulletin-9b31d.firebaseapp.com",
+  databaseURL: "https://ward-bulletin-9b31d.firebaseio.com",
+  projectId: "ward-bulletin-9b31d",
+  storageBucket: "ward-bulletin-9b31d.appspot.com",
+  messagingSenderId: "861011314549"
+};
+firebase.initializeApp(config);
+
+function startAuthUI(container) {
+  let uiConfig = {
+    // signInSuccessUrl: "<url-to-redirect-to-on-success>",
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      {
+        provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // Whether the display name should be displayed in the Sign Up page.
+        requireDisplayName: false
+      }
+      // firebase.auth.PhoneAuthProvider.PROVIDER_ID,
+      // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+    ],
+    credentialHelper: firebaseui.auth.CredentialHelper.NONE,
+    // tosUrl and privacyPolicyUrl accept either url string or a callback
+    // function.
+    // Terms of service url/callback.
+    // tosUrl: "<your-tos-url>",
+    // Privacy policy url/callback.
+    // privacyPolicyUrl: function() {
+    //   window.location.assign("<your-privacy-policy-url>");
+    // }
+    callbacks: {
+      signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+        // Process result. This will not trigger on merge conflicts.
+        // On success redirect to signInSuccessUrl.
+        setTimeout(() => {
+          ui.reset();
+        }, 500);
+        return true;
+      },
+      // signInFailure callback must be provided to handle merge conflicts which
+      // occur when an existing credential is linked to an anonymous user.
+      signInFailure: function(error) {
+        console.log("sign in error:", error);
+      }
+    }
+  };
+
+  // Initialize the FirebaseUI Widget using Firebase.
+  if (!ui) {
+    ui = new firebaseui.auth.AuthUI(firebase.auth());
+    // The start method will wait until the DOM is loaded.
+    ui.disableAutoSignIn();
+  }
+
+  ui.start("#" + container, uiConfig);
+}
+
+function logout() {
+  firebase.auth().signOut();
+}
+
+export default firebase;
+export { startAuthUI, logout };
--- src/routes/editor/index.js
+++ src/routes/editor/index.js
@@ -1,201 +1,65 @@
 import { h, Component } from "preact";
-import style from "./style";
-import BulletinData from "../../data/bulletindata";
 import { Page, Loader } from "../../components";
-import BulletinView from "../bulletin/bulletin-view";
-import EditorView from "./editor-view";
 import prefs from "../../data/prefs";
+import firebase, { startAuthUI } from "../../data/firebase";
+import "../../style/login-style.css";
+
+import EditorMain from "./editor-main";
+
+let containerId = "firebaseui-auth-container";
+let pageTitle = "Sign In";
 
 export default class Editor extends Component {
-  unit = "pq3";
-  state = { data: null };
-  undoStack = [];
+  state = { status: "checking" };
 
-  // gets called when this route is navigated to
   componentDidMount() {
-    // get data
-    let data = prefs.get(prefs.currentDraft);
-    if (data) {
-      // use current draft
-      this.setState({ data });
-    } else {
-      BulletinData.getBulletinData(this.unit).then(data => {
-        this.setState({ data });
-        prefs.set(prefs.currentDraft, data);
-      });
-    }
-  }
-
-  render({}, { data }) {
-    if (data) {
-      return (
-        <Page title="Editor">
-          <div class="fullheight" style={{ paddingBottom: "44px" }}>
-            <div
-              class="w3-row-padding w3-half fullheight w3-theme-d5"
-              style={{ overflow: "auto" }}
-            >
-              <EditorView
-                data={data}
-                update={request => {
-                  this.update(request);
-                  this.undoStack.push(request);
-                }}
-                change={request => {
-                  this.update(request);
-                }}
-              />
-            </div>
-            <div
-              class="w3-hide-small w3-half w3-white fullheight"
-              style={{ overflow: "auto" }}
-            >
-              <div class="w3-padding w3-white fullheight">
-                <BulletinView data={data} />
-              </div>
-            </div>
-            <Footer>
-              <button
-                onClick={e => {
-                  this.undo();
-                  e.stopPropagation();
-                }}
-                class={`w3-bar-item w3-button ${
-                  this.undoStack.length ? "" : "w3-disabled"
-                }`}
-              >
-                <i class="icon-ccw" />
-                Undo
-              </button>
-              <button
-                onClick={e => {
-                  e.stopPropagation();
-                }}
-                class="w3-bar-item w3-button"
-              >
-                <i class="icon-upload-cloud" />
-                Publish
-              </button>
-              <button
-                onClick={e => {
-                  e.stopPropagation();
-                }}
-                class="w3-bar-item w3-button"
-              >
-                <i class="icon-help-circled" />
-                Help
-              </button>
-            </Footer>
-          </div>
-        </Page>
-      );
-    } else {
-      // no data yet, show loader
-      return (
-        <Page title="Editor">
-          <Loader />
-        </Page>
-      );
-    }
-  }
-
-  update(request, undo = false) {
-    if (Array.isArray(request)) {
-      for (let thisRequest of request) {
-        // multiple requests
-        this.updateRequest(thisRequest, undo);
+    // see if we are logged in
+    firebase.auth().onAuthStateChanged(
+      user => {
+        this.setState({ status: user ? "loggedin" : "loggedout" });
+      },
+      error => {
+        this.error = error;
+        this.setState({ status: "error" });
+        console.log(error);
       }
-    } else {
-      this.updateRequest(request, undo);
+    );
+  }
+
+  componentDidUpdate() {
+    if (this.state.status === "loggedout") {
+      startAuthUI(containerId);
     }
   }
 
-  updateRequest(request, undo = false) {
-    const { type, index, sectionId, attr } = request;
-    let data = this.state.data.sections[sectionId].data;
-
-    switch (type) {
-      case "add":
-        if (undo) {
-          data.splice(index, 1);
-        } else {
-          data.splice(index, 0, request.item);
-        }
-        break;
-
-      case "update":
-        const item = data[index];
-        if (undo) {
-          item[attr] = request.oldValue;
-        } else {
-          request.oldValue = item[attr];
-          item[attr] = request.value;
-        }
-        break;
-
-      case "moveUp":
-        if (undo) {
-          data.splice(index, 0, data.splice(index - 1, 1)[0]);
-        } else {
-          data.splice(index - 1, 0, data.splice(index, 1)[0]);
-        }
-        break;
-
-      case "moveDown":
-        if (undo) {
-          data.splice(index, 0, data.splice(index + 1, 1)[0]);
-        } else {
-          data.splice(index + 1, 0, data.splice(index, 1)[0]);
-        }
-        break;
-
-      case "delete":
-        if (undo) {
-          data.splice(index, 0, request.oldValue);
-        } else {
-          const item = data[index];
-          request.oldValue = item;
-          data.splice(index, 1);
-        }
-        break;
+  render({}, { status }) {
+    switch (status) {
+      case "loggedin":
+        return <EditorMain />;
+      case "loggedout":
+        return (
+          <Page title={pageTitle}>
+            <div class="w3-margin-top w3-card w3-container w3-white w3-large">
+              <p>
+                Enter email and password to sign in or create a new account.
+              </p>
+            </div>
+            <div class="w3-margin-top" id={containerId} />
+          </Page>
+        );
+      case "error":
+        return this.error;
+      default:
+        // checking...
+        return (
+          <Page title={pageTitle}>
+            <Loader />
+          </Page>
+        );
     }
-
-    data = this.state.data;
-    this.setState(data);
-
-    // save in local storage
-    prefs.set(prefs.currentDraft, data);
-  }
-
-  undo() {
-    let request = this.undoStack.pop();
-    if (!request) {
-      return;
-    }
-    this.update(request, true);
   }
 }
 
-const Footer = ({ children }) => (
-  <header
-    class=""
-    style={{
-      opacity: 0.9,
-      position: "fixed",
-      bottom: "0px",
-      width: "100%",
-      opacity: ".9",
-      left: "0px"
-    }}
-  >
-    <div
-      class="w3-bar w3-content w3-theme-d2 w3-display-container"
-      style={{
-        display: "flex",
-        justifyContent: "space-around"
-      }}
-    >
-      {children}
-    </div>
-  </header>
-);
+function Login({}) {
+  return <Page title={pageTitle}>Login</Page>;
+}
