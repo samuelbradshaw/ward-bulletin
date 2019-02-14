b86c99f3f9b26deadee2e97bb3677663965e66db
stakedancecard <alanlbird@gmail.com>
2019-02-14 11:26:09 -0800

    New layout for Home screen

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,36 +6,62 @@ const Home = () => {
   let recents = prefs.get(prefs.recents);
   return (
     <Page title="Ward Bulletin App">
-      <ul class="w3-ul">
-        <p class="w3-margin-left w3-large">Find Bulletin</p>
-        <li class="w3-white" onClick={() => (location.href = "/locate")}>
-          Find at Current Location
-        </li>
-        <li class="w3-white" onClick={() => (location.href = "/search")}>
-          Search by Ward Name
-        </li>
-      </ul>
-
-      {recents && recents.length && (
-        <ul class="w3-ul">
-          <p class="w3-margin-left w3-large">Recent Bulletins</p>
-          {recents.map(ward => (
-            <li
-              class="w3-white"
-              onClick={() => (location.href = `/${ward.id}`)}
+      <div class="fullheight w3-white w3-container">
+        <h4>Welcome to the Ward Bulletin App</h4>
+        <div class="w3-card w3-container w3-section">
+          <p>
+            View your bulletin by using your current location to find the
+            closest bulletins or search using the name of your ward.
+          </p>
+          <div class="w3-margin">
+            <button
+              class="w3-btn w3-theme-d2 w3-round"
+              onClick={() => (location.href = "/locate")}
             >
-              {ward.name}
-            </li>
-          ))}
-        </ul>
-      )}
+              Find by Location
+            </button>
+          </div>
+          <div class="w3-margin">
+            <button
+              class="w3-btn w3-theme-d2 w3-round"
+              onClick={() => (location.href = "/search")}
+            >
+              Search by Name
+            </button>
+          </div>
+        </div>
 
-      <ul class="w3-ul">
-        <p class="w3-margin-left w3-large">Bulletin Editor</p>
-        <li class="w3-white" onClick={() => (location.href = "/editor")}>
-          Edit
-        </li>
-      </ul>
+        {recents && recents.length && (
+          <div class="w3-card w3-container w3-section">
+            <div class="w3-margin-top">Select a Recent Bulletin:</div>
+            <ul class="w3-ul w3-margin-bottom">
+              {recents.map(ward => (
+                <li
+                  class="w3-white w3-btn w3-round"
+                  onClick={() => (location.href = `/${ward.id}`)}
+                >
+                  {ward.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+
+        <div class="w3-card w3-container w3-section">
+          <p>
+            If you are a bulletin editor for your ward, login or create a new
+            account to edit your ward bulletin.
+          </p>
+          <div class="w3-margin">
+            <button
+              class="w3-btn w3-theme-d2 w3-round"
+              onClick={() => (location.href = "/editor")}
+            >
+              Edit Bulletin
+            </button>
+          </div>
+        </div>
+      </div>
     </Page>
   );
 };
