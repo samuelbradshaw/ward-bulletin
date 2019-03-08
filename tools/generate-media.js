83e5599102ae8305fbf3c8d8fd2a629e44d746d1
Alan Bird <alanlbird@gmail.com>
2019-03-07 16:31:00 -0800

    When generating media data, wasn't getting entire collection if contained more than 50 images

--- tools/generate-media.js
+++ tools/generate-media.js
@@ -1,8 +1,28 @@
-async function getMediaData(collection) {
-  const fetch = require("node-fetch");
+async function getMediaData(title, collection) {
   const url =
     "https://media-lib-api.lds.org/medialib/v1/media?language=eng" +
     (collection ? `&collectionId=${collection}` : "");
+  let json = await fetchMediaData(url);
+  let data = json.content;
+
+  while (json.numberOfElements === json.size) {
+    // get another page
+    json = await fetchMediaData(`${url}&start=${json.number + json.size}`);
+    data = data.concat(json.content);
+  }
+
+  // write raw data
+  const fs = require("fs");
+  fs.writeFileSync(
+    `dev/media-data/${title} ${collection}.json`,
+    JSON.stringify(data)
+  );
+
+  return data;
+}
+
+async function fetchMediaData(url) {
+  const fetch = require("node-fetch");
   let response = await fetch(url, {
     headers: {
       Authorization:
@@ -25,12 +45,12 @@ function findImageSize(thumbnails, size) {
       }
     }
   });
-  return result.url;
+  return result;
 }
 
 function saveFolder(title, id, items) {
   const fs = require("fs");
-  console.log(title, id);
+  // console.log(title, id);
   fs.writeFileSync(
     `src/assets/media-library/${id}.json`,
     JSON.stringify(items)
@@ -38,30 +58,53 @@ function saveFolder(title, id, items) {
   //   console.log(JSON.stringify(items));
 }
 
+function size(w, h) {
+  return `${w}x${h}`.padEnd(10);
+}
+
 async function processCollection(collection, title, id) {
   // save data
-  let items = collection.content.map(item => {
+  let items = collection.map(item => {
     if (item.classType === "COLLECTION") {
       let thumb = findImageSize(item.thumbnails, 200);
+      if (thumb.width && thumb.height) {
+        console.log(
+          size(thumb.width, thumb.height),
+          size(" ", " "),
+          `Collection: ${title} / ${item.title}`
+        );
+      }
+
       return {
         type: "folder",
         id: item.id,
         title: item.title,
-        thumb
+        thumb: thumb.url
       };
     } else if (item.classType === "ASSET") {
       let thumb = findImageSize(item.thumbnails, 200);
       let mobile = findImageSize(item.thumbnails, 768);
       let print = item.downloadUrl;
-      return { type: "image", title: item.title, thumb, mobile, print };
+      console.log(
+        size(thumb.width, thumb.height),
+        size(mobile.width, mobile.height),
+        `${title} / ${item.title}`
+      );
+      return {
+        type: "image",
+        title: item.title,
+        thumb: thumb.url,
+        mobile: mobile.url,
+        print
+      };
     }
   });
   saveFolder(title, id, items);
 
   // now get any sub collections
-  let collections = collection.content.map(async item => {
+  let collections = collection.map(async item => {
     if (item.classType === "COLLECTION") {
-      let subCollection = await getMediaData(item.id);
+      let subCollection = await getMediaData(item.title, item.id);
       await processCollection(subCollection, item.title, item.id);
     }
   });
@@ -69,10 +112,9 @@ async function processCollection(collection, title, id) {
 }
 
 async function getRootCollection() {
-  let data = await getMediaData();
-  let content = data.content;
+  let content = await getMediaData("Images");
   let images = content.find(item => item.title === "Images") || content[0];
-  let root = await getMediaData(images.id);
+  let root = await getMediaData(images.title, images.id);
   await processCollection(root, images.title, "images:eng");
 }
 
