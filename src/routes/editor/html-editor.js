d2a5341293608f0d2503bf2e536e936e6aa20ec1
Alan Bird <alanlbird@gmail.com>
2019-03-14 16:47:43 -0700

    Change font and font size in article

--- src/routes/editor/html-editor.js
+++ src/routes/editor/html-editor.js
@@ -106,6 +106,49 @@ class HTMLEditor extends Component {
             const url = window.prompt("Enter the link URL");
             if (url) exec("createLink", url);
           }
+        },
+        {
+          name: "font",
+          icon: `
+            <select id="font-select" onchange="this.parentElement.onclick(this)">
+              <option selected disabled>Font</option>
+              <option value="Arial">Arial</option>
+              <option value="Helvetica">Helvetica</option>
+              <option value="Times New Roman">Times New Roman</option>
+              <option value="Courier New">Courier New</option>/>
+              <option value="Verdana">Verdana</option>
+              <option value="Georgia">Georgia</option>/>
+              <option value="Palatino">Palatino</option>
+              <option value="Garamond">Garamond</option>
+              <option value="Bookman">Bookman</option>
+              <option value="Comic Sans MS">Comic Sans</option>
+              <option value="Trebuchet MS">Trebuchet MS</option>
+              <option value="Arial Black">Arial Black</option>
+              <option value="Impact">Impact</option>
+            </select>`,
+          result: () => {
+            let value = document.getElementById("font-select").value;
+            exec("fontName", value);
+          }
+        },
+        {
+          name: "size",
+          icon: `
+            <select id="fontsize-select" onchange="this.parentElement.onclick(this)">
+              <option selected disabled>Size</option>
+              <option value="1">Tiny</option>
+              <option value="2">Small</option>
+              <option value="3">Regular</option>
+              <option value="4">Bigger</option>
+              <option value="5">Large</option>
+              <option value="6">X-Large</option>
+              <option value="7">XX-Large</option>
+            </select>`,
+          result: () => {
+            let value = document.getElementById("fontsize-select").value;
+            console.log("Font size", value);
+            exec("fontSize", value);
+          }
         }
       ]
     });
