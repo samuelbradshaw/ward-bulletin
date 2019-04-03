e99032d2741a3ce5b7a18424ec2a0014723eec65
Alan Bird <alanlbird@gmail.com>
2019-04-03 15:38:58 -0700

    Update email and password

--- src/routes/editor/settings.js
+++ src/routes/editor/settings.js
@@ -1,10 +1,22 @@
 import { h, Component } from "preact";
 import prefs from "../../data/prefs";
-import { CheckBox, RadioButtons, NumberInput } from "../../components";
+import {
+  CheckBox,
+  RadioButtons,
+  NumberInput,
+  Label,
+  TextInput,
+  Button,
+  Loader
+} from "../../components";
+import firebase from "../../data/firebase";
 
 class Settings extends Component {
-  render({ update, settings }, { hideLabels }) {
+  render({ update, settings }, {}) {
     let autoDate = settings.autoDate || "Sunday";
+    let user = firebase.auth().currentUser;
+    this.email = user.email;
+
     return (
       <div>
         <h4>Settings</h4>
@@ -89,8 +101,78 @@ class Settings extends Component {
           </p>
           <p />
         </div>
+        <p />
+
+        <div class="w3-card w3-container">
+          <h5>Email and Password</h5>
+          <p>
+            Change your login email and/or password. If you are being replaced
+            by another bulletin editor, you may just change the email address,
+            then when your replacement logs in, they can click on the "Trouble
+            signing in?" link to reset the password.
+          </p>
+          <Label name="Current Email" />
+          <TextInput value={this.email} readonly />
+          <Label name="Current Password" />
+          <TextInput
+            type="password"
+            onChange={e => (this.password = e.target.value)}
+          />
+          <Label name="New Email" />
+          <TextInput onChange={e => (this.newEmail = e.target.value)} />
+          <Label name="New Password" />
+          <TextInput
+            type="password"
+            onChange={e => (this.newPassword = e.target.value)}
+          />
+          <p />
+          <Button onClick={e => this.updateSignin()}>Update</Button>
+        </div>
+        <p />
       </div>
     );
+  }
+
+  updateSignin() {
+    if (!this.password) {
+      alert("Please enter your current password");
+      return;
+    }
+    if (!this.newEmail && !this.newPassword) {
+      alert("Please enter a new email and/or new password");
+      return;
+    }
+
+    let user = firebase.auth().currentUser;
+    let credential = firebase.auth.EmailAuthProvider.credential(
+      this.email,
+      this.password
+    );
+
+    document.body.style.cursor = "wait";
+
+    user
+      .reauthenticateAndRetrieveDataWithCredential(credential)
+      .then(() => this.newEmail && user.updateEmail(this.newEmail))
+      .then(() => this.newPassword && user.updatePassword(this.newPassword))
+      .then(() =>
+        this.finishUpdateWithMessage(
+          this.newEmail && this.newPassword
+            ? "Updated email and password"
+            : this.newEmail
+            ? "Updated email"
+            : "Updated password"
+        )
+      )
+      .catch(
+        error => this.finishUpdateWithMessage(error.toString())
+        // An error happened.
+      );
+  }
+
+  finishUpdateWithMessage(message) {
+    document.body.style.cursor = "default";
+    alert(message);
   }
 }
 
--- src/routes/home/test.js
+++ src/routes/home/test.js
@@ -1,16 +1,25 @@
 import { h, Component } from "preact";
 import { Page } from "../../components";
-import SaveTemplate from "../editor/save-template";
+import Settings from "../editor/settings";
 import prefs from "../../data/prefs";
 
 const Test = () => {
   let unit = prefs.get(prefs.draftId);
   let data = prefs.get(prefs.draftBulletin);
 
+  let settings = {
+    leaderchar: " .",
+    name: "",
+    printColumns: 2,
+    centerMargin: 0.75,
+    edgeMargin: 0.25,
+    autoDate: "Sunday"
+  };
+
   return (
     <Page title="Test Page">
-      <div class="w3-container">
-        <SaveTemplate unit={unit} data={data} />
+      <div class="w3-container w3-white">
+        <Settings settings={settings} />
       </div>
     </Page>
   );
