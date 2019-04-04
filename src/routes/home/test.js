6a0d898cce5a39c940bb66743e34433d6a31dcd6
Alan Bird <alanlbird@gmail.com>
2019-04-03 17:42:16 -0700

    Using editor settings in test route caused home and bulletin routes to be very large

--- src/routes/home/test.js
+++ src/routes/home/test.js
@@ -1,25 +1,15 @@
 import { h, Component } from "preact";
 import { Page } from "../../components";
-import Settings from "../editor/settings";
 import prefs from "../../data/prefs";
 
 const Test = () => {
   let unit = prefs.get(prefs.draftId);
   let data = prefs.get(prefs.draftBulletin);
 
-  let settings = {
-    leaderchar: " .",
-    name: "",
-    printColumns: 2,
-    centerMargin: 0.75,
-    edgeMargin: 0.25,
-    autoDate: "Sunday"
-  };
-
   return (
     <Page title="Test Page">
       <div class="w3-container w3-white">
-        <Settings settings={settings} />
+        <h3>Test</h3>
       </div>
     </Page>
   );
